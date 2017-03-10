f0f67c33f5f0ff1a776397bd5b028df5d0aafe19
adedayo ayodele <adedayo.ayodele@blogeeria.com>
2017-03-10 14:29:43 +0100

    'okay'

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');  
 var request = require('request');  
 var app = express();
+var started=false;
 
 app.use(bodyParser.urlencoded({extended: false}));  
 app.use(bodyParser.json());  
@@ -9,14 +10,156 @@ app.listen((process.env.PORT || 3000));
 
 // Server frontpage
 app.get('/', function (req, res) {  
-    res.send('This is TestBot Server');
+    res.send('Test Bot');
 });
 
 // Facebook Webhook
+
 app.get('/webhook', function (req, res) {  
     if (req.query['hub.verify_token'] === 'testbot_verify_token') {
         res.send(req.query['hub.challenge']);
     } else {
-        res.send('Invalid verify token');
+        res.send('Invalid verify token supplied');
     }
 });
+
+app.post('/webhook', function (req, res) { 
+	if(!started){
+		//welcomeUser();
+		//curl -X GET "https://graph.facebook.com/v2.6/<USER_ID>?fields=first_name,last_name,profile_pic,locale,timezone,gender&access_token=PAGE_ACCESS_TOKEN" 
+	}
+	
+    var events = req.body.entry[0].messaging;
+    for (i = 0; i < events.length; i++) {
+        var event = events[i];
+		/*
+        if (event.message && event.message.text) {
+            sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+        }
+		*/
+		/*
+		if (event.message && event.message.text) {  
+			if (!kittenMessage(event.sender.id, event.message.text)) {
+				sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+			}
+		}
+		*/
+				
+		if (event.message && event.message.text) {  
+			if (!welcomeMessage(event.sender.id, event.message.text)) {
+				if (!kittenMessage(event.sender.id, event.message.text)) {
+					sendMessage(event.sender.id, {text: "" + event.message.text});
+				}
+			}
+			
+		} else if (event.postback) {
+			console.log("Postback received: " + JSON.stringify(event.postback));
+		}
+				
+    }
+    res.sendStatus(200);
+});
+
+
+// generic function sending messages
+function sendMessage(recipientId, message) {  
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+        method: 'POST',
+        json: {
+            recipient: {id: recipientId},
+            message: message,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+};
+
+
+function kittenMessage(recipientId, text) {
+
+    text = text || "";
+    var values = text.split(' ');
+
+    if (values.length === 3 && values[0] === 'kitten') {
+        if (Number(values[1]) > 0 && Number(values[2]) > 0) {
+
+            var imageUrl = "https://placekitten.com/" + Number(values[1]) + "/" + Number(values[2]);
+
+            message = {
+                "attachment": {
+                    "type": "template",
+                    "payload": {
+                        "template_type": "generic",
+                        "elements": [{
+                            "title": "Kitten",
+                            "subtitle": "Cute kitten picture",
+                            "image_url": imageUrl ,
+                            "buttons": [{
+                                "type": "web_url",
+                                "url": imageUrl,
+                                "title": "Show kitten"
+                                }, {
+                                "type": "postback",
+                                "title": "I like this",
+                                "payload": "User " + recipientId + " likes kitten " + imageUrl,
+                            }]
+                        }]
+                    }
+                }
+            };
+
+            sendMessage(recipientId, message);
+
+            return true;
+        }
+    }
+
+    return false;
+
+};
+
+
+function welcomeMessage(recipientId, text) {
+	text = text || "";
+    var values = text.split(' ');
+
+    if (values.length === 2 && values[0] === 'get' && values[1] === 'started') {
+
+            message = {
+                "attachment": {
+                    "type": "template",
+                    "payload": {
+                        "template_type": "generic",
+                        "elements": [{
+                            "title": "Surrogate app lets you optimize the time you spend on Facebook to learn something useful.",
+                            "buttons": [{
+								"type": "postback",
+                                "title": "Let's Go",
+                                "payload": "start_me",
+                                }, {
+                                "type": "postback",
+                                "title": "About",
+                                "payload": "about_me",
+                                }, {
+								"title": "Help",
+                                "type": "postback",
+                                "payload": "help_me",
+                            }]
+                        }]
+                    }
+                }
+            };
+			sendMessage(recipientId, message);
+            return true;
+    }
+
+    return false;
+          //  sendMessage(recipientId, message);
+
+};
