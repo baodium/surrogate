eeb40c6ca31dcd318ae3bff6e20eae3fd320f68c
adedayo ayodele <adedayo.ayodele@blogeeria.com>
2017-03-10 23:37:49 +0100

    'ok'

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.get('/', function (req, res) {
 			});
 		*/	
 			
-		client.query("CREATE TABLE users (id bigint(20) NOT NULL PRIMARY KEY AUTO_INCREMENT, facebook_id varchar(200) NOT NULL,name varchar(1024) NOT NULL,sex varchar(512) DEFAULT NULL,age varchar(512) DEFAULT NULL,email varchar(512) DEFAULT NULL,date_added datetime DEFAULT NULL,updated_at datetime NOT NULL);",  function (err, result) {
+		client.query("create table `category` (`id` int auto_increment,`name` char(10),`description` varchar(45),primary key (`id`)) comment = 'expense category records' ;",  function (err, result) {
 		if (err) {
 			res.send("Error connecting");
 		}
