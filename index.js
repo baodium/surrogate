96ce31c0208a71f82c3d40687037cdcbdd937423
adedayo ayodele <adedayo.ayodele@blogeeria.com>
2017-03-15 06:50:52 +0100

    'cool'

--- index.js
+++ index.js
@@ -76,7 +76,8 @@ app.get('/webhook', function (req, res) {
 });
 
 app.post('/webhook', function (req, res) { 
-	showMenu();
+	//showMenu();
+	addPersistentMenu();
     var events = req.body.entry[0].messaging;
     for (i = 0; i < events.length; i++) {
         var event = events[i];
@@ -273,7 +274,7 @@ function about(recipientId) {
                             "title": "About message",
                             "buttons": [{
 								"type": "postback",
-                                "title": "About",
+                                "title": "I got it!",
                                 "payload": "quit_help_about",
                                 }]
                         }]
@@ -308,6 +309,44 @@ function help(recipientId) {
 			
             return false;		
 };
+
+function addPersistentMenu(){
+ request({
+    url: 'https://graph.facebook.com/v2.6/me/thread_settings',
+    qs: { access_token: PAGE_ACCESS_TOKEN },
+    method: 'POST',
+    json:{
+        setting_type : "call_to_actions",
+        thread_state : "existing_thread",
+        call_to_actions:[
+            {
+              type:"postback",
+              title:"Home",
+              payload:"home"
+            },
+            {
+              type:"postback",
+              title:"Joke",
+              payload:"joke"
+            },
+            {
+              type:"web_url",
+              title:"DMS Software Website",
+              url:"http://www.dynamic-memory.com/"
+            }
+          ]
+    }
+
+}, function(error, response, body) {
+    console.log(response)
+    if (error) {
+        console.log('Error sending messages: ', error)
+    } else if (response.body.error) {
+        console.log('Error: ', response.body.error)
+    }
+})
+
+}
 
 function showMenu(){
   var menu =  {
