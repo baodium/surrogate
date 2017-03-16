312126c0dccc1c03085095d4bd4c3cd45789c2dc
adedayo ayodele <adedayo.ayodele@blogeeria.com>
2017-03-16 08:15:57 +0100

    ok

--- index.js
+++ index.js
@@ -33,8 +33,14 @@ app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging;
     for (i = 0; i < events.length; i++) {
 		addPersistentMenu();
+		
         var event = events[i];						
 		if (event.message && event.message.text) {
+			if(!started){
+				displayWelcomeMessage(event.sender.id);
+				started = true;
+			}
+			
 				var text = event.message.text;
 				text = text || "";
 				var values = text.split(' ');
