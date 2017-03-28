cacbd6d1fa11ca5ecf0eca79268be333ef333a4b
adedayo ayodele <adedayo.ayodele@blogeeria.com>
2017-03-28 08:10:44 +0100

    ok

--- index.js
+++ index.js
@@ -410,6 +410,7 @@ function sendAcceptance(fromId,requestId,senderId){
 			
 			sendMessage(senderId, {text: name+" is now your "+subject+" student."});
 			messageOption(senderId,"Do you want to message him?",senderId,fromId,subject);
+			//recipientId,msg,fromm,to,subject
 			
 			//senderContext[senderId].firstName+" "+senderContext[senderId].lastName
 			
@@ -417,10 +418,10 @@ function sendAcceptance(fromId,requestId,senderId){
 			messageOption(fromId,"Do you want to message him?",fromId,senderId,subject);
 						
 			var p_data = querystring.stringify({'request_id' : reqId,'status':'completed'});
-			submitForm(p_data,backurl+"requests/update",senderId,"update_request2");
+			//submitForm(p_data,backurl+"requests/update",senderId,"update_request2");
 						
 		}catch(err){
-			sendMessage(senderId, {text: body+""});  
+			sendMessage(senderId, {text: " exception "});  
 		}       		
 		}
 		});
