4a176b55a173edba26d4f461cc0432747b7bff51
adedayo ayodele <adedayo.ayodele@blogeeria.com>
2017-03-28 07:41:36 +0100

    ok

--- index.js
+++ index.js
@@ -149,14 +149,17 @@ app.post('/webhook', function (req, res) {
 					 sendRejection(fromId,expertiseId,event.sender.id);
 				}
 				
-			}else if(reply.payload.indexOf("accept_request")>-1){				
+			}else if(reply.payload.indexOf("accept_request")>-1){
+				sendMessage(event.sender.id, {text: "I will selected now"});				
+				/*
 				var expertise_id = reply.payload.split("-");
 				 expertiseId = expertise_id[1];
 				 fromId = expertise_id[2];
-				 sendMessage(event.sender.id, {text: "I will selected"});
+				 sendMessage(event.sender.id, {text: "I will selected now"});
 				 if(senderContext[event.sender.id]!=null){  
-					 sendAcceptance(fromId,expertiseId,event.sender.id);
+					// sendAcceptance(fromId,expertiseId,event.sender.id);
 				}
+				*/
 				
 			}else if(reply.payload=="home"){
 				welcomeUser(event.sender.id);				
@@ -404,16 +407,11 @@ function sendAcceptance(fromId,requestId,senderId){
 			reqId = bodyObject.request_id;
 			
 			sendMessage(senderId, {text: name+" is now your "+subject+" student."});
-			//messageOption(senderId,"Do you want to message him?",senderId,fromId,subject);
+			messageOption(senderId,"Do you want to message him?",senderId,fromId,subject);
 			
-			/*sendMessage(fromId, {text: senderContext[senderId].firstName+" "+senderContext[senderId].lastName+" has accepted your "+subject+" expertise request. He's now in your expert list."});
-			messageOption(fromId,"Do you want to message him?",fromId,senderId,subject);
-			*/
 			sendMessage(fromId, {text: senderContext[senderId].firstName+" "+senderContext[senderId].lastName+" has accepted your "+subject+" expertise request. He's now in your expert list."});
-			sendMessage(senderId, {text: senderContext[senderId].firstName+" "+senderContext[senderId].lastName+" has accepted your "+subject+" expertise request. He's now in your expert list."});
-			//messageOption(senderId,"Do you want to message him?",fromId,senderId,subject);
-			
-			
+			messageOption(fromId,"Do you want to message him?",fromId,senderId,subject);
+						
 			var p_data = querystring.stringify({'request_id' : reqId,'status':'completed'});
 			submitForm(p_data,backurl+"requests/update",senderId,"update_request2");		
 		}catch(err){
@@ -899,7 +897,7 @@ function submitForm(post_data,url,userId,action){
 								ownerId = senderContext[userId].requestTo;
 								requestId = senderContext[userId].expertiseId;
 								sendMessage(userId, {text: "Your request has been sent. Hopefully, you will get a reply very soon."});				
-								sendMessage(userId, {text: "You have a new request. "+name+" wants to learn "+subject+" from you"});	
+								//sendMessage(userId, {text: "You have a new request. "+name+" wants to learn "+subject+" from you"});	
 								sendMessage(ownerId, {text: "You have a new request. "+name+" wants to learn "+subject+" from you"});									
 								
 								message = {"attachment": {
