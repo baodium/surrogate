075c393659bb15ff5beafb10a6f27f44124a034b
adedayo ayodele <adedayo.ayodele@blogeeria.com>
2017-03-30 10:03:21 +0100

    ok

--- index.js
+++ index.js
@@ -1245,9 +1245,11 @@ function submitForm(post_data,url,userId,action){
 								pickTime(userId);
 								senderContext[userId].status="pick_reminder_time";
 							}else{
-								var period = post_data.split("&");
-								//period  = period.split("-");
-								if(sendMessage(userId, {text: "You have already set up a reminder for "+post_data+" \n\n please select another day"})){
+								var period = post_data.split("=");
+								period = preiod[period.length-1];
+								period = period.split("_");
+								period = period[1].toLowerCase();
+								if(sendMessage(userId, {text: "You have already set up a reminder for "+period+" \n\n please select another day"})){
 									pickPeriod(userId);
 								}
 							}
