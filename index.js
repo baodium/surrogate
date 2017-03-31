2243fc2efee391df5e9f7a2df74da04a8e4d20ee
adedayo ayodele <adedayo.ayodele@blogeeria.com>
2017-03-31 15:01:20 +0100

    ok

--- index.js
+++ index.js
@@ -57,15 +57,11 @@ app.get('/EAAJeiL9sIu4BANZAqkGafo', function (req, res) {
 // Facebook Webhook
 
 app.get('/webhook', function (req, res) {  
-    var events = req.body;//.entry[0].messaging;
-	res.send(events);
-	/*
     if (req.query['hub.verify_token'] === 'testbot_verify_token') {
         res.send(req.query['hub.challenge']);
     } else {
         res.send('Invalid verify token supplied');
     }
-	*/
 });
 
 app.post('/webhook', function (req, res) { 
