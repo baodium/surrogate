c2abb1066c2e7ebe97093ea4d39819bd02c2968a
adedayo ayodele <adedayo.ayodele@blogeeria.com>
2017-04-08 09:38:24 +0100

    cool

--- index.js
+++ index.js
@@ -261,7 +261,7 @@ app.post('/webhook', function (req, res) {
 						defaultMsg ="And you too, "+senderContext[event.sender.id].firstName+". \n\n";
 					}else if(contains.call(greetings_pool, msgin)){
 						defaultMsg ="I'm cool! "+senderContext[event.sender.id].firstName+". \n\n";
-					}else if(msgin.indexOf("damn")>-1 || msgin.indexOf("fuck")>-1 || msgin.indexOf("insane")>-1 || msgin.indexOf("crazy")>-1 || msgin.indexOf("mad")>-1 ){					
+					}else if(msgin.indexOf("damn")>-1 || msgin.indexOf("fuck")>-1 || msgin.indexOf("fool")>-1 || msgin.indexOf("insane")>-1 || msgin.indexOf("crazy")>-1 || msgin.indexOf("mad")>-1 ){					
 						defaultMsg ="Oh "+senderContext[event.sender.id].firstName+", that is not a very nice thing to say. \n\n Maybe you will feel better by providing help to someone on a subject you are proficient at. \n\n ";
 					}
 										
