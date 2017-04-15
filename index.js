ac68bf931378705e63a3319fe39085f7b38d2d5c
adedayo ayodele <adedayo.ayodele@blogeeria.com>
2017-04-15 14:25:23 +0100

    ok

--- index.js
+++ index.js
@@ -771,7 +771,7 @@ function checkHelper(subject,senderId){
 					elementss[i]={                           
 							"title": output[i].name, 
 							"image_url": output[i].profile_pic,                  
-							"subtitle":  "Expert in:"+output[i].subject+", \n\n Level:"+level+"",   
+							"subtitle":  "Expert in:"+output[i].subject+", \n\n Level:"+level+"\n\n "+"ðŸ‘©",   
                             "buttons": [{
 											"title": "Request Expertise",
 											"type": "postback",
