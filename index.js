4339a77e4f125d7218dbd4058a08b6645efc9166
adedayo ayodele <adedayo.ayodele@blogeeria.com>
2017-04-16 19:22:34 +0100

    ok

--- index.js
+++ index.js
@@ -295,8 +295,6 @@ app.post('/webhook', function (req, res) {
 						defaultMsg ="I'm cool! "+senderContext[event.sender.id].firstName+". \n\n";
 					}else if(msgin.indexOf("damn")>-1 || msgin.indexOf("fuck")>-1 || msgin.indexOf("fool")>-1 || msgin.indexOf("insane")>-1 || msgin.indexOf("crazy")>-1 || msgin.indexOf("mad")>-1 ){					
 						defaultMsg ="Oh "+senderContext[event.sender.id].firstName+", that is not a very nice thing to say. \n\n Maybe you will feel better by providing help to someone on a subject you are proficient at. \n\n ";
-					}else if(msgin=="share it"){
-						shareIt(event.sender.id);
 					}
 										
 					sendMessage(event.sender.id, {text: "" + defaultMsg+"This is what I have on my menu "});
@@ -1162,7 +1160,7 @@ function showMenu(recipientId){
                     }
                 }
             };
-			
+			shareIt(recipientId);
 			sendMessage(recipientId, message);
 			return true;
 }
