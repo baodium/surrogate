0ea02118065bd426c5dc17ad30789d41e6ec2008
adedayo ayodele <adedayo.ayodele@blogeeria.com>
2017-04-20 15:10:51 +0100

    ok

--- index.js
+++ index.js
@@ -1274,7 +1274,7 @@ function about(recipientId) {
         "template_type":"generic",
         "elements":[
           {
-            "title":"💡 Surrogate Bot",
+            "title":"Surrogate Bot 💡",
             "subtitle":"Assist students learn from experts and allow experts render help on messenger.",
 			"image_url": "http://surrogation.com.ng/surrogateapp/resources/images/logo.png",
             "buttons":[
