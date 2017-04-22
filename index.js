e33062e4b9138a0fbca6d477a9d8fed8f37905ef
adedayo ayodele <adedayo.ayodele@blogeeria.com>
2017-04-22 14:15:28 +0100

    ok

--- index.js
+++ index.js
@@ -871,9 +871,11 @@ function checkHelper(subject,senderId,page){
 							"title":"🎓 "+subject+" request list",
 							"subtitle": "Here is what I found",								
 					};
-					//var i=(page>0)?1:0;
-					
-					for(i=0; i<j ; i++){					
+					page = parseFloat(page);
+					var i=(page>0)?1:0;
+					if(i<j){
+						i++;
+					//for(i=0; i<j ; i++){					
 					//for(i = 0; i<output.length; i++){
 						level = output[i].level;//.split("_");
 						if(level!=null){
